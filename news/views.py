780e3e92fe851444c0efe77b0393f827483e091a
den503 <zaychuk.dd@gmail.com>
2020-04-05 17:20:56 +0300

    updated api for get one news

--- news/views.py
+++ news/views.py
@@ -6,4 +6,5 @@ from .models import News
 class NewsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
     queryset = News.objects.filter(is_active=True)
     serializer_class = NewsSerializer
+    lookup_field = 'slug'
     permission_classes = (permissions.AllowAny, )
