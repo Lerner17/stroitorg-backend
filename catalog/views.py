4f6200c8a081bec9c66519fcc7179f59c4f63fd1
den503 <zaychuk.dd@gmail.com>
2020-04-11 13:42:58 +0300

    added pagination for products

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
 from rest_framework import permissions, viewsets, mixins
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 
@@ -7,6 +10,30 @@ class ProductViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.Retr
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (permissions.AllowAny, )
+    pagination_class = PageNumberPagination
+    DEFAULT_PAGINATION_COUNT = 10
+    ordering_fields = ['id']
+
+    def get_serializer_class(self):
+        return self.serializer_class
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        self.pagination_class.page_size = self.get_paginated_count()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_paginated_count(self):
+        count = self.request.GET.get('count')
+        if count is None:
+            return self.DEFAULT_PAGINATION_COUNT
+        else:
+            return count
 
 
 class CategoryViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
