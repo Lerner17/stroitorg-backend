aa44f4282ba53374950e8bf2872f5df0bc1a75a6
den503 <zaychuk.dd@gmail.com>
2020-04-12 12:46:20 +0300

    added search api for product

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, viewsets, mixins
+from rest_framework import permissions, viewsets, mixins, filters
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -7,6 +7,8 @@ from .serializers import ProductSerializer, CategoryListSerializer, CategoryDeta
 
 
 class ProductViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    search_fields = ['name', 'description', 'category__name']
+    filter_backends = (filters.SearchFilter, )
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (permissions.AllowAny, )
