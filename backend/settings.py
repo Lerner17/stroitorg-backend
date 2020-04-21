57295809c8b22a992842f319ef96bbb00162f674
den503 <zaychuk.dd@gmail.com>
2020-04-21 21:04:37 +0300

    updated settings permission

--- backend/settings.py
+++ backend/settings.py
@@ -112,8 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.IsAuthenticated',
-        # 'rest_framework.permissions.AllowAny',
         # 'rest_framework.permissions.IsAdminUser',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
