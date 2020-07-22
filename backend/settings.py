882c5480b86aa4c84c09054f1cdc15b4452d35d7
Иван <lerner@mbp-1.local>
2020-07-22 18:33:16 +0300

    Fix mistake in settings

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't%6#v7^#9ayg*^4mhfw43_v2^-u+nz)v-g97o=9xk!biumzj39'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', FALSE)
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
